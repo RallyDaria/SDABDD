cd9fd2c79eff64c875dda5140f165e445b7bae68
RallyDaria <dariazblewska@gmail.com>
2022-11-09 18:12:08 +0100

    wpisywanie hasła loogowanie oraz zamykanie przeglądarki

--- src/test/java/Steps/Logowanie.java
+++ src/test/java/Steps/Logowanie.java
@@ -3,9 +3,12 @@ package Steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class Logowanie {
 
@@ -14,7 +17,7 @@ public class Logowanie {
     @Given("Uzytkownik otwiera przegladarke")
     public void testUzytkownikOtwieraPrzegladarke(){
         System.out.println("Uzytkownik otworzyl przegladarke");
-        System.setProperty("webdriver.chrome.driver","C:/Users/zblewski/Desktop/chromdriver/chromedriver107.exe");
+        //System.setProperty("webdriver.chrome.driver","C:/Users/zblewski/Desktop/chromdriver/chromedriver107.exe");
          driver = new ChromeDriver();
          driver.manage().window().maximize();
     }
@@ -33,15 +36,21 @@ public class Logowanie {
     @When("Uzytkownik wpisuje poprawne haslo")
     public void uzytkownik_wpisuje_poprawne_haslo() {
         System.out.println("Uzytkowanik wpisuje poprawne haslo");
+        WebElement inputPassword = driver.findElement(By.id("password"));
+        inputPassword.sendKeys("SuperSecretPassword!");
     }
     @When("Uzytkownik klika przycisk Login")
     public void uzytkownik_klika_przycisk_login() {
         System.out.println("Uzytkownik klika przycisk Login");
+        driver.findElement(By.tagName("button")).click();
+
     }
 
     @Then("Uzytkownik zostal poprawnie zalogowany")
     public void uzytkownik_zostal_poprawnie_zalogowany() {
         System.out.println("Uzytkownik zostal poprawnie zalogowany");
+        Assert.assertEquals("https://the-internet.herokuapp.com/secure", driver.getCurrentUrl());
+        driver.close();
     }
 
 }
